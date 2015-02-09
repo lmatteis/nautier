adef94cb4cedb161b7e3bb3fe3e382311a457759
lmatteis@gmail.com <lmatteis@gmail.com>
2015-02-09 16:46:48 +0100

    fin

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 KeywordNavigation(
     ['http://dbpedia.org/resource/Keanu_Reeves'],
-    ['the matrix'],
+    ['starring'],
     0,
     []
 );
@@ -11,60 +11,71 @@ function KeywordNavigation(inputURIs, keywords,
         MP = [],
         currKeyword = keywords[currKeywordIdx];
     if(!currKeyword) {
-        console.log(MT)
         return MT;
     }
 
     MT = [];
+
+    MT_unique = {}
+    T_unique = {}
+    P_unique = {}
+    MP_unique = {}
     for(var i=0; i<inputURIs.length; i++) {
         var uri = inputURIs[i];
         dereference(uri, function(triples) {
             for(var i=0; i<triples.length; i++) {
                 var triple = triples[i];
-                T.push(triple);
                 T_push(triple);
             }
             predicateResolution(triples, function(t) {
-                P.push(t);
                 P_push(t);
             });
             edgeResolution(uri, triples, function(t) {
-                T.push(t);
                 T_push(t);
             });
         });
     }
     function T_push(triple) {
+        if(T_unique[triple.subject + triple.predicate + triple.object])
+            return;
+        T.push(triple);
+        T_unique[triple.subject + triple.predicate + triple.object] = true;
         if(contains(triple, currKeyword)) {
-            MT.push(triple);
             MT_push(triple);
         }
     }
     function P_push(triple) {
+        if(P_unique[triple.subject + triple.predicate + triple.object])
+            return;
+        P.push(triple);
+        P_unique[triple.subject + triple.predicate + triple.object] = true;
         if(contains(triple, currKeyword)) {
-            MP.push(triple);
             MP_push(triple);
         }
     }
-    function MP_push(M_triple) {
+    function MP_push(triple) {
+        if(MP_unique[triple.subject + triple.predicate + triple.object])
+            return;
+        MP.push(triple);
+        MP_unique[triple.subject + triple.predicate + triple.object] = true;
         for(var i=0; i<T.length; i++) {
             var T_triple = T[i];
-            if(T_triple.predicate == M_triple.subject) {
-                MT.push(T_triple);
+            if(T_triple.predicate == triple.subject) {
                 MT_push(T_triple);
             }
         }
     };
-    MT_unique = {}
     function MT_push(triple) {
         if(MT_unique[triple.subject + triple.predicate + triple.object])
             return;
+        MT.push(triple);
         MT_unique[triple.subject + triple.predicate + triple.object] = true;
         var uris = getSubjectObjectUris(triple);
         for(var i in uris) {
             var uri = uris[i]
             if(!(inputURIs.indexOf(uri) > -1)) {
                 if(uri.indexOf('http') == 0) {
+                    console.log(uri)
                     KeywordNavigation([uri], keywords,
                                       currKeywordIdx + 1, MT);
                 }
